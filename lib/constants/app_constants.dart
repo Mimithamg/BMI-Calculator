03212675bf1f22b4799526f9df2cea815cabdd89
Mimithamg <114565866+Mimithamg@users.noreply.github.com>
2023-04-13 21:58:30 +0530

    Add files via upload

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final Color mainHexColor = HexColor("#03324a");
+final Color accentHexColor = HexColor("#acb7ff");
